43ea596aef5da790149a26c71143d675a6ba899d
pacoo <pacoo@DESKTOP-9A4R88P>
2026-03-19 19:37:55 +0100

    feat: añadir estructura inicial del proyecto

---
+++ Proyecto_Mantenimiento/src/main/java/com/example/demo/ProyectoMantenimientoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMantenimientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMantenimientoApplication.class, args);
+	}
+
+}
