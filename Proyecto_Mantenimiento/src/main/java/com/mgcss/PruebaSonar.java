fe09cb6484be49e1570b5e6fa345df3260e1846a
Francisco <francisco.orrego@alu.uhu.es>
2026-03-26 17:44:54 +0100

    test: simular reduccion de coverage para comprobar bloqueo

---
+++ Proyecto_Mantenimiento/src/main/java/com/mgcss/PruebaSonar.java
@@ -0,0 +1,11 @@
+package com.mgcss; 
+
+public class PruebaSonar {
+    public String simularFallo(boolean activado) {
+        if (activado) {
+            return "El coverage va a caer";
+        } else {
+            return "La aduana me va a bloquear";
+        }
+    }
+}
