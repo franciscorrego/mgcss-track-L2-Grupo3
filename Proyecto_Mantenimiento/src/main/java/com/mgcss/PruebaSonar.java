6ebe86051a8875083bf1b825a226fbd91c3aefb6
Francisco <francisco.orrego@alu.uhu.es>
2026-03-26 17:48:20 +0100

    test: forzar bloqueo superando las 20 lineas sin coverage

--- Proyecto_Mantenimiento/src/main/java/com/mgcss/PruebaSonar.java
+++ Proyecto_Mantenimiento/src/main/java/com/mgcss/PruebaSonar.java
@@ -1,11 +1,24 @@
-package com.mgcss; 
+package com.mgcss;
 
 public class PruebaSonar {
-    public String simularFallo(boolean activado) {
-        if (activado) {
-            return "El coverage va a caer";
-        } else {
-            return "La aduana me va a bloquear";
-        }
+    
+    public String simularFallo1(boolean activado) {
+        if (activado) { return "El coverage va a caer"; } else { return "Bloqueo 1"; }
+    }
+    
+    public String simularFallo2(boolean activado) {
+        if (activado) { return "El coverage va a caer"; } else { return "Bloqueo 2"; }
+    }
+    
+    public String simularFallo3(boolean activado) {
+        if (activado) { return "El coverage va a caer"; } else { return "Bloqueo 3"; }
+    }
+    
+    public String simularFallo4(boolean activado) {
+        if (activado) { return "El coverage va a caer"; } else { return "Bloqueo 4"; }
+    }
+
+    public String simularFallo5(boolean activado) {
+        if (activado) { return "El coverage va a caer"; } else { return "Bloqueo 5"; }
     }
 }
